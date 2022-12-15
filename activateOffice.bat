30671779bf1370ed05fb2bf271f1a0f05260063e
Jacob Edwards <j4ke95@gmail.com>
2022-12-15 11:52:22 +0000

    Initial Commit

---
+++ activateOffice.bat
@@ -0,0 +1,5 @@
+cd "C:\Program Files\Microsoft Office\Office16"
+cscript ospp.vbs /inpkey:11111-11111-11111-11111-11111
+cscript ospp.vbs /sethst:127.0.0.1
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /act
---
+++ activateWin.bat
@@ -0,0 +1,3 @@
+slmgr.vbs /ipk 11111-11111-11111-11111-11111
+slmgr.vbs /skms 127.0.0.1:1688
+slmgr.vbs /ato
